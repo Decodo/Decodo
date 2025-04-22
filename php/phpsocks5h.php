6bf6c58b8cb4f6a7cb0d37d0e68e903999e29e55
Žilvinas <159907476+sakanamk@users.noreply.github.com>
2025-04-22 11:41:00 +0300

    Update phpsocks5h.php

--- php/phpsocks5h.php
+++ php/phpsocks5h.php
@@ -1,7 +1,7 @@
 <?php
 
-    $url = 'ip.smartproxy.com/json';
-    $proxy = 'https://gate.smartproxy.com';
+    $url = 'ip.decodo.com/json';
+    $proxy = 'https://gate.decodo.com';
     $port = 7000;
     $user = 'username';
     $psw = 'password';
